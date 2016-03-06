422398ceaff562f2723eb433a56f2f240fc38306
David Sudia <dsudia@gmail.com>
2016-03-06 16:59:44 -0700

    setup knex

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var router = express.Router();
+var knex = require('../db/knex');
 
 router.get("/", function(request, response, next) {
     response.render("index");
