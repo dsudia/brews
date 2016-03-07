c24b7dff4d70c41c6e1de41786a326d7e9bb3e3b
David Sudia <dsudia@gmail.com>
2016-03-06 18:25:03 -0700

    setup for deployment on heroku

--- knexfile.js
+++ knexfile.js
@@ -2,5 +2,9 @@ module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://localhost:5432/brews'
+  },
+  publication: {
+    client: 'pg',
+    connection: 'postgres://pbiligtqtlpymi:JpI8coC_orMzDdsISdPkp4fS-s@ec2-54-204-7-145.compute-1.amazonaws.com:5432/dfobce8vp6ctq0?ssl=true'
   }
 };
