7d1a6d9d9e482dc8008070d8e7d156f2b6289848
David Sudia <dsudia@gmail.com>
2016-03-06 17:21:42 -0700

    beers seed file works correctly with foreign keys

--- seeds/2_beers.js
+++ seeds/2_beers.js
@@ -5,9 +5,9 @@ exports.seed = function(knex, Promise) {
     knex('beers').del(),
 
     // Inserts seed entries
-    knex('beers').insert({brewery_id: 1, name: 'Yellow Fever', abv: 5.0}),
-    knex('beers').insert({brewery_id: 1, name: 'Redcon', abv: 5.7}),
-    knex('beers').insert({brewery_id: 2, name: 'True Blonde Ale', abv: 5.0}),
-    knex('beers').insert({brewery_id: 2, name: 'Modus Hoperandi IPA', abv: 6.8})
+    knex('beers').insert({brewery_id: knex.select('id').table('breweries').where('name', 'Comrade'), name: 'Yellow Fever', abv: 5.0}),
+    knex('beers').insert({brewery_id: knex.select('id').table('breweries').where('name', 'Comrade'), name: 'Redcon', abv: 5.7}),
+    knex('beers').insert({brewery_id: knex.select('id').table('breweries').where('name', 'Ska'), name: 'True Blonde Ale', abv: 5.0}),
+    knex('beers').insert({brewery_id: knex.select('id').table('breweries').where('name', 'Ska'), name: 'Modus Hoperandi IPA', abv: 6.8})
   );
 };
