c07003ed9a5ed9f6f7cc635f07ee003c9942b5da
David Sudia <dsudia@gmail.com>
2016-03-06 18:50:58 -0700

    took out ssl=true

--- knexfile.js
+++ knexfile.js
@@ -5,6 +5,6 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: 'postgres://pbiligtqtlpymi:JpI8coC_orMzDdsISdPkp4fS-s@ec2-54-204-7-145.compute-1.amazonaws.com:5432/dfobce8vp6ctq0?ssl=true'
+    connection: 'postgres://pbiligtqtlpymi:JpI8coC_orMzDdsISdPkp4fS-s@ec2-54-204-7-145.compute-1.amazonaws.com:5432/dfobce8vp6ctq0'
   }
 };
